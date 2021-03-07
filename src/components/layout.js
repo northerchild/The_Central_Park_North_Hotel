f3c8d30b44d3b223f545f2ed5eed80c9f8a482e1
northerchild <osarus-13@hotmail.com>
2021-03-06 22:51:20 -0500

    App finalizada

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,55 +1,55 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+import React from 'react';
+import Header from './header'
+import {Global,css} from '@emotion/react';
+import Helmet from 'react-helmet';
+import Footer from './footer';
+import useSeo from '../hooks/use-seo';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+const Layout = (props) => {
+    const seo = useSeo();
+    const {fallbackSeo:{description,title}} = seo;
+    return (
+        <>
+            <Global
+                styles={css`
+                    html{
+                        font-size:62.5%;
+                    }
+                    body{
+                        font-size:1.6px;
+                        font-size:1.6rem;
+                        line-height:1.5;
+                    }
+                    h1,h2,h3{
+                        margin: 0;
+                        line-height:1.5;
+                    }
+                    h1,h2{
+                        font-family:'Roboto', serif;
+                    }
+                    h3{
+                        font-family: 'PT Sans', sans-serif;
+                    }
+                    ul{
+                        list-style:none;
+                        margin: 0;
+                        padding: 0;
+                    }                    
+                `
+                }
+            />
+            <Helmet>
+                <title>{title}</title>
+                <meta name="description" content={description}/>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&display=swap" />
+            </Helmet>
+            <Header/>
+            {props.children}
+            <Footer title={title}/>
+        </>  
+        );
 }
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+ 
+export default Layout;
---
+++ src/pages/nosotros.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ContenidoNosotros from "../components/contenidoNosotros"
+import Layout from "../components/layout"
+
+const Nosotros = () => (
+  <Layout>
+      <ContenidoNosotros/>
+  </Layout>
+)
+
+export default Nosotros
